dd694abd293fc189e282fd29ccdc8435b8d34fba
RichenelWartim <114988676+RichenelWartim@users.noreply.github.com>
2023-03-13 11:09:30 +0100

    Add files via upload

--- Project/SendCode.php
+++ Project/SendCode.php
@@ -16,26 +16,23 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 
     <nav class="navbar navbar-expand-lg" style="background-color: #2A1C3D;">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a class="fw-bold navbar-brand" href="index.php" style="color:white;">Total Code B.V.</a>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-left: 650px;">
-                    <li class="nav-item">
-                        <a class="fw-bold nav-link" href="SendCode.php" style="color:white;text-decoration:underline">Send Code</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="fw-bold nav-link" style="color:white;">|</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="fw-bold nav-link" href="RetrieveCode.php" style="color:white;">Retrieve Code</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="fw-bold navbar-brand" href="index.php" style="color:white;margin-right:35%;">TotalCode B.V.</a>
+        <div>
+          <a class="fw-bold navbar-brand" href="SendCode.php" style="color:white;text-decoration:underline">Send Code</a>
+          <a class="fw-bold navbar-brand" href="SendCode.php" style="color:white;">|</a>
+          <a class="fw-bold navbar-brand" href="RetrieveCode.php" style="color:white;">Retrieve Code</a>
         </div>
-    </nav>
+
+      </div>
+    </div>
+
+    </div>
+  </nav>
     <div class="container-fluid" style="margin-top: 2%;">
         <div class="row">
             <div class="col-sm-5">
@@ -54,10 +51,10 @@
             <div class="col-sm-4">
                 <div class="w-100 p-5" style="background-color:#533E6D; height:700px;">
                     <label for="inputPassword5" class="fw-bold form-label" style="color:white;">Name:</label>
-                    <input type="text" id="inputPassword5" class="form-control" style="background-color:#684F86;" aria-describedby="passwordHelpBlock">
+                    <input type="text" id="inputPassword5" class="fw-bold form-control" style="background-color:#684F86; color:white;" aria-describedby="passwordHelpBlock">
                     <br>
                     <label for="inputPassword5" class="fw-bold form-label" style="color:white;">Code title:</label>
-                    <input type="text" id="CodeTitle" class="form-control" style="background-color:#684F86;" aria-describedby="passwordHelpBlock">
+                    <input type="text" id="CodeTitle" class="fw-bold form-control" style="background-color:#684F86;color:white;" aria-describedby="passwordHelpBlock">
                     <br>
                     <div class="form-floating">
                         <textarea class="fw-bold form-control" style="height:400px; background-color:#684F86; color:white;" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
