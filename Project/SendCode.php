f275d60999e650ad156af028ea9f1f476194ac52
justinhorizon <114988813+justinhorizon@users.noreply.github.com>
2023-03-15 12:21:40 +0100

    commit

--- Project/SendCode.php
+++ Project/SendCode.php
@@ -1,29 +1,29 @@
 <?php
 
-session_start();
-
 $host = 'localhost';
-$db   = 'project_pastebin';
-$user = 'project_pastebin';
-$pass = 'Pr0ject';
+$db = 'project_pastebin';
+$user = 'bit_academy';
+$pass = 'bit_academy';
+
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 
-// shows version of the database
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
+  $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
-    echo 'error connecting to database :( on line : ' . $e->getMessage();
+  throw new \PDOException($e->getMessage(), (int)$e->getCode());
+
 }
 
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -40,7 +40,7 @@ try {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
-    <script src="script.js"></script>
+
     <nav class="navbar navbar-expand-lg" style="background-color: #2A1C3D;">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
@@ -62,20 +62,17 @@ try {
             </div>
         </div>
     </nav>
-    <code>
+    <form action="SendConfirm.php" method="post">
         <div class="container-fluid" style="margin-top: 2%;">
             <div class="row">
                 <div class="col-sm-5">
-                    <div class="w-100 p-5 " style="background-color:#533E6D; height:700px;">
-                        <h1 class="fw-bold text-white " style="font-size: 20px; margin-left: 35%;">Voer hier de code in:</h1>
+                    <div class="w-100 p-5 " style="background-color:#533E6D ; height:700px;">
+                        <h1 class="fw-bold text-white " style="font-size: 20px; margin-left: 35%;">Paste your code here</h1>
                         <div class="col-sm-12">
                             <div class="form-floating">
                                 <textarea class="fw-bold form-control" style="height: 580px; background-color:#684F86; color:white;" placeholder="Leave a comment here" id="code" name="code"></textarea>
                                 <label class="fw-bold" for="code" style="color:white;">Code</label>
                             </div>
-                        </div>
-                        <div class="col-sm-12 mt-3" style="display: flex; justify-content: center;">
-                            <button type="button" id="submit-button" class="fw-bold" style="background-color:#533E6D; color:white; border-color:white; width:100%; align-items:center;">Post code</button>
                         </div>
                     </div>
                 </div>
@@ -83,64 +80,31 @@ try {
                 </div>
                 <div class="col-sm-4">
                     <div class="w-100 p-5" style="background-color:#533E6D; height:700px;">
-                        <form>
-                            <label for="inputPassword5" class="fw-bold form-label" style="color:white;">Name:</label>
-                            <input type="text" name="Author" id="name" class="form-control" style="background-color:#684F86;" aria-describedby="passwordHelpBlock">
-                            <br>
-                            <label for="inputPassword5" class="fw-bold form-label" style="color:white;">Code title:</label>
-                            <input type="text" name="Title" id="CodeTitle" class="form-control" style="background-color:#684F86;" aria-describedby="passwordHelpBlock">
-                            <br>
-                            <div class="form-floating">
-                                <textarea name="CodeFuntion" class="fw-bold form-control" style="height:400px; background-color:#684F86; color:white;" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-                                <label for="floatingTextarea" class="fw-bold" style="color:white;">Code Funtion</label>
-                            </div>
-                        </form>
+                        <label for="inputPassword5" class="fw-bold form-label" style="color:white;">Name:</label>
+                        <input type="text" name="Author" id="name" class="form-control" style="background-color:#684F86;" aria-describedby="passwordHelpBlock">
+                        <br>
+                        <label for="inputPassword5" class="fw-bold form-label" style="color:white;">Code title:</label>
+                        <input type="text" name="Title" id="CodeTitle" class="form-control" style="background-color:#684F86;" aria-describedby="passwordHelpBlock">
+                        <br>
+                        <div class="form-floating">
+                            <textarea name="CodeFuntion" class="fw-bold form-control" style="height:400px; background-color:#684F86; color:white;" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                            <label for="floatingTextarea" class="fw-bold" style="color:white;">Code Funtion</label>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <!-- Voeg hier eventuele JavaScript-bestanden toe -->
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
-        <script>
-            $(document).ready(function() {
-                $('#code-input').keypress(function(event) {
-                    if (event.keyCode === 13) {
-                        event.preventDefault();
-                        $('#submit-button').click();
-                    }
-                });
+            <div class="container-fluid" style="margin-top: 2%;">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="col-sm-12">
+                            <input class="fw-bold" style="background-color:#533E6D; color:white; width:100%; height:70px;align-items:center;" type="submit" value="Post code" id="PostCode" name="PostCode">
+                            <!-- <button class="fw-bold" style="background-color:#533E6D; color:white; width:100%; height:70px;align-items:center;">Post code</button> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </form>
 
-                $('#submit-button').click(function() {
-                    var code = $('#code-input').val();
-                    if (code.trim() == '') {
-                        alert('Voer een code in.');
-                        return;
-                    }
-                    var form = $('<form>', {
-                        'action': 'pagina2.php',
-                        'method': 'POST'
-                    }).append($('<input>', {
-                        'type': 'hidden',
-                        'name': 'code',
-                        'value': code
-                    }));
-                    $('body').append(form);
-                    form.submit();
-                });
-            });
-        </script>
-        <script>
-            // Highlight de code
-            $(document).ready(function() {
-                var code = $('#code').text()
-                hljs.highlight('php'.code);
-                hljs.highlight('css', code);
-                hljs.highlight('html', code);
-                hljs.highlight('sql', code);
-            });
-        </script>
-    </code>
 </body>
 
 </html>
--- Project/SendConfirm.php
+++ Project/SendConfirm.php
@@ -1,3 +1,46 @@
+<?php
+
+$host = 'localhost';
+$db = 'project_pastebin';
+$user = 'bit_academy';
+$pass = 'bit_academy';
+
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+  $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+  throw new \PDOException($e->getMessage(), (int)$e->getCode());
+
+}
+
+session_start();
+$author = $_POST['Author'];
+$title = $_POST['Title'];
+$Function = $_POST['CodeFuntion'];
+$Code = $_POST['code'];
+
+$pdo = new PDO($dsn, $user, $pass);
+
+
+$sql = "INSERT INTO code_table(code_author, code_title, code_function, code)
+  VALUES (?, ?, ?, ?);";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute([$author, $title, $Function, $Code]);
+
+header('Refresh:3; url=index.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,43 +54,30 @@
 </head>
 
 <body style="background-color: #392A4D;">
-  <?php
-
-  header('Refresh:3; url=index.php');
-  ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 
-  <nav class="navbar navbar-expand-lg" style="background-color: #2A1C3D;">
+  <nav class="navbar navbar-expand-lg" style="background-color:#2A1C3D;">
     <div class="container-fluid">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <a class="fw-bold navbar-brand" href="index.php" style="color:white;margin-right:35%;">TotalCode B.V.</a>
-        <div>
-          <a class="fw-bold navbar-brand" href="SendCode.php" style="color:white;text-decoration:underline">Send Code</a>
-          <a class="fw-bold navbar-brand" href="SendCode.php" style="color:white;">|</a>
-          <a class="fw-bold navbar-brand" href="RetrieveCode.php" style="color:white;">Retrieve Code</a>
-        </div>
-
+        <a class="navbar-brand" href="index.php" style="color:white;">TotalCode B.V.</a>
       </div>
-    </div>
-
-    </div>
   </nav>
   <div class="container-fluid" style="margin-top: 15%;">
     <div class="row">
       <div class="col-sm-1">
       </div>
       <div class="col-sm-10">
-        <div class="w-100 p-5 " style="background-color:#533E6D ; height:100%;">
-          <h1 class="fw-bold text-white " style="font-size: 70px; margin-left: 30%; margin-top: 60px;  ">code has been posted</h1>
-        </div>
+          <div class="w-100 p-5 " style="background-color:#533E6D ; height:350px;">
+            <h1 class="fw-bold text-white " style="font-size: 70px; margin-left: 300px; margin-top: 60px;  ">code has been posted</h1>
       </div>
-      <div class="col-sm-1">
-      </div>
+    </div>
+    <div class="col-sm-1">
+    </div>
 
 </body>
 
