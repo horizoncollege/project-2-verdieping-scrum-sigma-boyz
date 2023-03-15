2ba9636190e56c2c434345d6a7e4833825bb0b51
RichenelWartim <114988676+RichenelWartim@users.noreply.github.com>
2023-03-15 12:07:17 +0100

    fixed it so it is the same as the design plan

--- Project/RetrieveCode.php
+++ Project/RetrieveCode.php
@@ -1,10 +1,9 @@
 <?php
 
 $host = 'localhost';
-$db = 'project_pastebin';
-$user = 'bit_academy';
-$pass = 'bit_academy';
-
+$db   = 'project_pastebin';
+$user = 's168308_Project';
+$pass = 'Pr0ject';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
@@ -18,7 +17,6 @@ try {
   $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
   throw new \PDOException($e->getMessage(), (int)$e->getCode());
-
 }
 
 $stmt = $pdo->prepare("SELECT * FROM code_table");
@@ -32,19 +30,22 @@ if (isset($_GET['search'])) {
   $code_array = $stmtsearch->fetchAll(PDO::FETCH_OBJ);
 }
 
-function zoektable($code_array) {
+function zoektable($code_array)
+{
   foreach ($code_array as $key) {
     echo
     '<tr>
-      <td>';
-    echo $key->code_title;
-    echo '</td>
-      <td>';
-    echo $key->code_author;
-    echo '</td>
-      <td><a href="CodeDetails.php?id=';
+      <td> <a style="text-decoration:none; color:white; font-weight:bold"';
+    echo 'href="CodeDetails.php?id=';
     echo $key->code_id;
-    echo '">Zie code</a></td>
+    echo '">';
+    echo $key->code_title;
+    echo '</a></td>
+    <td><a style="text-decoration:none; color:white; font-weight:bold;" ';
+    echo ' href="CodeDetails.php?id=';
+    echo $key->code_id;
+    echo '">';
+    echo $key->code_author;'</a></td>
   </tr>';
   }
 }
@@ -70,45 +71,44 @@ function zoektable($code_array) {
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 
+
   <nav class="navbar navbar-expand-lg" style="background-color: #2A1C3D;">
     <div class="container-fluid">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <a class="fw-bold navbar-brand" href="index.php" style="color:white;">Total Code B.V.</a>
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-left: 650px;">
-          <li class="nav-item">
-            <a class="fw-bold nav-link" href="SendCode.php" style="color:white;">Send Code</a>
-          </li>
-          <li class="nav-item">
-            <a class="fw-bold nav-link" style="color:white;">|</a>
-          </li>
-          <li class="nav-item">
-            <a class="fw-bold nav-link" href="RetrieveCode.php" style="color:white;text-decoration:underline">Retrieve Code</a>
-          </li>
-        </ul>
+      <div class="collapse navbar-collapse " id="navbarTogglerDemo01">
+        <a class="fw-bold navbar-brand" href="index.php" style="color:white;margin-right:35%;">TotalCode B.V.</a>
+        <div>
+          <a class="fw-bold navbar-brand" href="SendCode.php" style="color:white;">Send Code</a>
+          <a class="fw-bold navbar-brand" href="SendCode.php" style="color:white;">|</a>
+          <a class="fw-bold navbar-brand" href="RetrieveCode.php" style="color:white;text-decoration:underline">Retrieve Code</a>
+        </div>
+
       </div>
+    </div>
+
     </div>
   </nav>
   <div class="col-sm-1">
   </div>
   <br>
-  <form id="zoek" method="get" action="RetrieveCode.php">
-    <input type="text" name="search" placeholder="Search the database">
-    <input type="submit" value="search">
-  </form>
+  <div class="col-sm-10">
+    <form class="d-flex" id="zoek" method="get" action="RetrieveCode.php">
+      <input class="fw-bold form-control me-2" type="text" name="search" placeholder="Search the database" style="margin-left: 18%; background-color:#2A1C3D; color:white;">
+      <input class="btn btn-outline-success" type="submit" value="search">
+    </form>
+  </div>
   <br>
   <div class="col-sm-10">
     <table class="table table-bordered" style="background-color:#47365B; color:white; margin-left:10%;">
       <tr>
         <th>Code title</th>
         <th>Code author</th>
-        <th>More details</th>
       </tr>
 
       <tr>
-        <?php 
+        <?php
         zoektable($code_array);
         ?>
       </tr>
